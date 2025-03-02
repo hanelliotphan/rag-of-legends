13aa32e0d34a917acd892c7c156b18cf8e267b2e
Han-Elliot Phan <hanelliotphan@gmail.com>
2025-03-01 20:05:03 -0800

    Add functions to get embeddings clear existing vector store and

--- rag-of-legends/src/model/langchain.py
+++ rag-of-legends/src/model/langchain.py
@@ -24,6 +24,7 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders import DirectoryLoader, TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.schema import Document
+from langchain_core.callbacks import StdOutCallbackHandler
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_chroma import Chroma
 from langchain.memory import ConversationBufferMemory
@@ -43,6 +44,28 @@ def collect_knowledge(folder_path):
             doc.metadata['doc_type'] = doc_type
             documents.append(doc)
     return documents
+
+
+def collect_knowledge_document_chunks(documents):
+    """
+    collect_knowledge_document_chunks -- Get chunks from knowledge base 
+    documents
+    """
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = text_splitter.split_documents(documents=documents)
+    return chunks
+
+
+def get_embeddings():
+    return OpenAIEmbeddings(api_key=os.environ.get("OPENAI_API_KEY"))
+
+
+def clear_chroma_vector_store(embeddings, db_name):
+    """
+    clear_chroma_vector_store -- Clear existing Chroma vector store
+    """
+    if os.path.exists(db_name):
+        Chroma(persist_directory=db_name, embedding_function=embeddings).delete_collection()
 
 
 def create_chroma_vector_store(doc_chunks, embeddings, db_name):
@@ -74,5 +97,6 @@ def create_conversation_chain(vector_store):
     return ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=retriever,
-        memory=memory
+        memory=memory,
+        callbacks=[StdOutCallbackHandler()]
     )
